aa229f846892451925409aedeb77ecd605eeefc3
NEU-ZYXi <stephenxi@hotmail.com>
2018-11-27 14:07:57 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include("includes/header.php");
+
+// query in the database, specify the connection and write the SQL
+// $query = mysqli_query($con, "INSERT INTO test VALUES('', 'Steven')");
+// $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Steven')"); 
+
+?>
+
+	Hello World
+</body>
+</html>
