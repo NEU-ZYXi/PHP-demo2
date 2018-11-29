48ad73b6595579e1b314d33a4403c325d9045704
NEU-ZYXi <stephenxi@hotmail.com>
2018-11-29 02:31:31 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,34 @@ include("includes/header.php");
 
 ?>
 
-	Hello World
+	<div class="user_details column">
+		<a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['profile_pic']; ?>" width="200" height="auto"></a>
+
+		<div class="user_details_left_right">
+			<a id="profile_name" href="<?php echo $userLoggedIn; ?>">
+				<?php 
+					echo $user['first_name'] . " " . $user['last_name'] . "<br>";
+				 ?>
+			</a>
+
+			<?php 
+				echo $user['username'] . "<br><br>";
+				echo "Posts: " . $user['num_posts'] . "<br>"; 
+				echo "Likes: " . $user['num_likes'];
+			?>
+		</div>
+
+	</div>
+
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="What's on your mind?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<hr>
+		</form>
+	</div>	
+
+</div>
+
 </body>
 </html>
