ee0d1eda629cbe359b0140b0058f407f2451c057
NEU-ZYXi <stephenxi@hotmail.com>
2018-11-29 02:30:53 -0800

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+include("includes/header.php");
+
+// query in the database, specify the connection and write the SQL
+// $query = mysqli_query($con, "INSERT INTO test VALUES('', 'Steven')");
+// $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Steven')"); 
+
+?>
+
+
+	<div class="main_column column">
+		This is a profile page
+	</div>	
+
+</div>
+
+</body>
+</html>
