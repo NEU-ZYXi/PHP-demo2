ad5340265a3291c8800551f59ed682027b8e4904
NEU-ZYXi <stephenxi@hotmail.com>
2018-12-04 00:11:55 -0800

    Add files via upload

---
+++ Demo/includes/classes/User.php
@@ -0,0 +1,111 @@
+<?php 
+
+// PHP OOP
+class User {
+
+	// fields
+	private $user;
+	private $con;
+
+	// constructor
+	public function __construct($con, $username) {
+		$this->con = $con;
+		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+		$this->user = mysqli_fetch_array($user_details_query);
+	}
+
+	public function getFirstAndLastName() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['first_name']. " " . $row['last_name'];
+	}
+
+	public function getUsername() {
+		return $this->user['username'];
+	}
+
+	public function getNumPosts() {
+		$username = $this->getUsername();
+		$query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['num_posts'];
+	}
+
+	public function isActive() {
+		$username = $this->getUsername();
+		$query = mysqli_query($this->con, "SELECT user_active FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+
+		if ($row['user_active'] == 'yes') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function isFriend($username_to_check) {
+		$usernameComma = "," . $username_to_check . ",";
+
+		// check if the friend username is in the friend array or it's the own username
+		if ((strstr($this->user['friend_array'], $usernameComma) || $username_to_check == $this->user['username'])) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function getProfilePic() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT profile_pic FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['profile_pic'];
+	}
+
+	public function getFriendArray() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['friend_array'];
+	}
+
+	public function didReceiveRequest($user_from) {
+		$user_to = $this->user['username'];
+		$check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'" );
+		if (mysqli_num_rows($check_request_query) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function didSendRequest($user_to) {
+		$user_from = $this->user['username'];
+		$check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'" );
+		if (mysqli_num_rows($check_request_query) > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function removeFriend($user_to_remove) {
+		$logged_in_user = $this->user['username'];
+
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$user_to_remove'");
+		$row = mysqli_fetch_array($query);
+		$friend_array_username = $row['friend_array'];
+
+		$new_friend_array = str_replace($user_to_remove . ",", "", $this->user['friend_array']);
+		$remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$logged_in_user'");
+		$new_friend_array = str_replace($this->user['username'] . ",", "", $friend_array_username);
+		$remove_friend = mysqli_query($this->con, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
+	}
+
+	public function sendRequest($user_to) {
+		$user_from = $this->user['username'];
+		$query = mysqli_query($this->con, "INSERT INTO friend_requests VALUES('', '$user_to', '$user_from')");
+	}
+}
+
+ ?>
---
+++ Demo/index.php
@@ -0,0 +1,98 @@
+<?php
+include("includes/header.php");
+
+// query in the database, specify the connection and write the SQL
+// $query = mysqli_query($con, "INSERT INTO test VALUES('', 'Steven')");
+// $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Steven')"); 
+
+if (isset($_POST['post'])) {
+	$post = new Post($con, $userLoggedIn);
+	$post->submitPost($_POST['post_text'], 'none');
+}
+
+?>
+
+	<div class="user_details column">
+		<a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['profile_pic']; ?>" width="200" height="auto"></a>
+
+		<div class="user_details_left_right">
+			<a id="profile_name" href="<?php echo $userLoggedIn; ?>">
+				<?php 
+					echo $user['first_name'] . " " . $user['last_name'] . "<br>";
+				 ?>
+			</a>
+
+			<?php 
+				echo $user['username'] . "<br><br>";
+				echo "Posts: " . $user['num_posts'] . "<br>"; 
+				echo "Likes: " . $user['num_likes'];
+			?>
+		</div>
+
+	</div>
+
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="What's on your mind?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<hr>
+		</form>
+
+		 <div class="posts_area"></div>
+		 <!-- show the icon in the AJAX part -->
+		 <img id="loading" src="assets/images/icons/loading.gif">  
+	</div>	
+
+	<script>
+		var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+		$(document).ready(function() {
+			$('#loading').show();
+
+			// original AJAX request for loading first posts
+			$.ajax({
+				url: "includes/handlers/ajax_load_posts.php",
+				type: "POST",
+				data: "page=1&userLoggedIn=" + userLoggedIn, 
+				cache: false,
+
+				success: function(data) {
+					$('#loading').hide();
+					$('.posts_area').html(data);
+				}
+			});
+
+			$(window).scroll(function() {
+				var height = $('.posts_area').height();  // div containing posts
+				var scroll_top = $(this).scrollTop();
+				var page = $('.posts_area').find('.nextPage').val();
+				var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+				if ((document.body.scrollHeight <= window.scrollY + window.innerHeight) && noMorePosts == 'false') {
+					$('#loading').show();
+
+					var ajaxReq = $.ajax({
+						url: "includes/handlers/ajax_load_posts.php",
+						type: "POST",
+						data: "page=" + page + "&userLoggedIn=" + userLoggedIn, 
+						cache: false,
+
+						success: function(response) {
+							$('.posts_area').find('.nextPage').remove();  // remove current next page
+							$('.posts_area').find('.noMorePosts').remove();
+
+							$('#loading').hide();
+							$('.posts_area').append(response);
+						}
+					});
+				}
+
+				return false;
+			});
+		});
+	</script>
+
+</div>
+
+</body>
+</html>
